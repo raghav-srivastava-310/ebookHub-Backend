3a6dfbf00ae606e25ac20c2b0be7902d388e53e6
Raghav Srivastava <raghavsri305@gmail.com>
2026-04-28 21:17:47 +0530

    Solve Session Problem

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use(cors({
   origin:process.env.FRONTEND_URL,
   credentials:true
 }))
+app.set('trust proxy', 1);
 
 app.use(session({
   secret:"captchaSecretKey",
