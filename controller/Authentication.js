1da4e4b34220997471a43ef5768229ffdaab8212
Raghav Srivastava <raghavsri305@gmail.com>
2026-04-29 11:20:15 +0530

    Cookies Set Production

--- controller/Authentication.js
+++ controller/Authentication.js
@@ -1,112 +1,116 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 import UserModel from "../model/User.js"
-import {OAuth2Client} from "google-auth-library"
+import { OAuth2Client } from "google-auth-library"
 import svgCaptcha from "svg-captcha"
-export const signup = async (req,res)=>{
-try {
-    const {name,email,password} = req.body;
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
     // if(userCaptcha !== req.session.captcha){
     //   return res.status(400).json({message:"Invalid Captcha",success:false})
     // }
     // req.session.captcha = null;
-  const user = await UserModel.findOne({email});
-  if(user){
-    return res.status(403).json({message:"User Already Exist",success:false});
-  }
-  const hasspass = await bcrypt.hash(password,10);
-  const newUser = await UserModel.create({
-    name,
-    email,
-    password:hasspass
-  });
-  return res.status(201).json({message:"Signup Successfully",success:true})
-} catch (error) {
-  console.log(error.message);
-  return res.status(500).json({message:"Internal Server Error",success:false})
-}
-}
-export const signin = async (req,res)=>{
-  try {
-    const {email,password} = req.body;
-    const user = await UserModel.findOne({email});
-    if(!user){
-      return res.status(403).json({message:"User Not Found Please Signup",success:false})
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      return res.status(403).json({ message: "User Already Exist", success: false });
     }
-    const isPass = await bcrypt.compare(password,user.password);
-    if(!isPass){
-      return res.status(401).json({message:"The credential Are not matched",success:false});
-    }
-    const accessToken = jwt.sign({email:user.email,name:user.name},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1d"})
-    res.cookie("accessToken",accessToken,{
-      httpOnly:true,
-      secure:false,
-      sameSite:"lax",
-      maxAge:24*60*60*1000
-    })
-    return res.status(200).json({message:"Signin Successfully",success:true,accessToken})
+    const hasspass = await bcrypt.hash(password, 10);
+    const newUser = await UserModel.create({
+      name,
+      email,
+      password: hasspass
+    });
+    return res.status(201).json({ message: "Signup Successfully", success: true })
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json({message:"Internal Server Error",success:false})
+    return res.status(500).json({ message: "Internal Server Error", success: false })
+  }
+}
+export const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(403).json({ message: "User Not Found Please Signup", success: false })
+    }
+    const isPass = await bcrypt.compare(password, user.password);
+    if (!isPass) {
+      return res.status(401).json({ message: "The credential Are not matched", success: false });
+    }
+    const accessToken = jwt.sign({ email: user.email, name: user.name }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000
+    })
+    return res.status(200).json({ message: "Signin Successfully", success: true, accessToken })
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal Server Error", success: false })
   }
 }
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-export const googleAuth = async (req,res)=>{
+export const googleAuth = async (req, res) => {
   try {
-    const {token} = req.body;
+    const { token } = req.body;
     const ticket = await client.verifyIdToken({
-      idToken:token,
-      audience:process.env.GOOGLE_CLIENT_ID
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID
     })
-    const {name,email} = ticket.getPayload();
-    const user = await  UserModel.findOne({email});
-    if(user){
-      const accessToken = jwt.sign({email:user.email,name:user.name},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1d"})
-      res.cookie("accessToken",accessToken,{
-        httpOnly:true,
-        secure:false,
-        sameSite:"lax",
-        maxAge:24*60*60*1000
+    const { name, email } = ticket.getPayload();
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      const accessToken = jwt.sign({ email: user.email, name: user.name }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 24 * 60 * 60 * 1000
       })
-      return res.status(200).json({message:"Signin Successfully",success:true})
+      return res.status(200).json({ message: "Signin Successfully", success: true })
     }
     const newUser = await UserModel.create({
       name,
       email,
-      password:""
+      password: ""
     })
-    const accessToken = jwt.sign({email:newUser.email,name:newUser.name},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1d"})
-    res.cookie("accessToken",accessToken,{
-      httpOnly:true,
-      secure:false,
-      sameSite:"lax",
-      maxAge:24*60*60*1000
-    })
-    return res.status(201).json({message:"Signup Successfully",success:true})
+    const accessToken = jwt.sign({ email: newUser.email, name: newUser.name }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
+    const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000
+    });
+    return res.status(201).json({ message: "Signup Successfully", success: true })
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json({message:"Internal Server Error",success:false})
+    return res.status(500).json({ message: "Internal Server Error", success: false })
   }
 }
-export const getCaptcha = (req,res)=>{
+export const getCaptcha = (req, res) => {
   const captcha = svgCaptcha.create({
-    size:6,
-    noise:3,
-    color:false,
-    background:"#827272",
+    size: 6,
+    noise: 3,
+    color: false,
+    background: "#827272",
   });
   req.session.captcha = captcha.text;
   res.type("svg");
   res.status(200).send(captcha.data);
 };
-export const getUser = async (req,res)=>{
+export const getUser = async (req, res) => {
   try {
-    const {email,name} = req.user
-  return res.status(203).json({message:"User fetched",success:true,userDetail:{
-    userEmail:email,
-    userName:name
-  }})
+    const { email, name } = req.user
+    return res.status(203).json({
+      message: "User fetched", success: true, userDetail: {
+        userEmail: email,
+        userName: name
+      }
+    })
   } catch (error) {
-    console.log("Error while fetch user:",error.message);
+    console.log("Error while fetch user:", error.message);
   }
 }
