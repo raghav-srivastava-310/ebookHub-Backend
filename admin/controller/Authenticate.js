6cc28f9e0e2ae01621286b82fad1df136eb0a3f0
Raghav Srivastava <raghavsri305@gmail.com>
2026-05-07 12:23:35 +0530

    Add logout Function

--- admin/controller/Authenticate.js
+++ admin/controller/Authenticate.js
@@ -41,3 +41,16 @@ export const getAdminDetails = async (req,res)=>{
     res.status(500).json({ message: error.message,success:false });
   }
 }
+export const logout = async (req,res)=>{
+  try {
+    res.clearCookie("adminToken",{
+      httpOnly:true,
+      secure:process.env.NODE_ENV === "production",
+      sameSite:process.env.NODE_ENV === "production" ? "none" : "lax",
+    });
+    res.status(200).json({message:"Logged out successfully",success:true});
+  }
+    catch (error) { 
+      res.status(500).json({ message: error.message,success:false });
+    }
+}
--- admin/routes/Authenticate.js
+++ admin/routes/Authenticate.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAdminDetails, login } from "../../admin/controller/Authenticate.js"
+import { getAdminDetails, login, logout } from "../../admin/controller/Authenticate.js"
 import AuthenticateAdmin from "../middleware/AuthenticatAdmin.js";
 
 const router = Router();
 
 router.post("/login",login);
 router.get("/get-admin",AuthenticateAdmin,getAdminDetails);
+router.post("/logout",AuthenticateAdmin,logout);
 
 export default router;
